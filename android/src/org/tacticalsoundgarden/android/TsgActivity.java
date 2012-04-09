cf749616f7e9f9e4709fb072fcc5197dfbfb37af
Mark Shepard <mark.w.shepard@gmail.com>
2012-04-09 17:47:01 -0400

    initial commit of phonegap jquery mobile code

---
+++ android/src/org/tacticalsoundgarden/android/TsgActivity.java
@@ -0,0 +1,13 @@
+package org.tacticalsoundgarden.android;
+
+import android.os.Bundle;
+import org.apache.cordova.*;
+
+public class TsgActivity extends DroidGap {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.loadUrl("file:///android_asset/www/index.html");
+    }
+}
